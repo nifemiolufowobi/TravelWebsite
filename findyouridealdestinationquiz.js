aea55427f457f0b4d389d91297f93d4c8c170587
nifemiolufowobi <ono@njit.edu>
2024-11-20 16:25:54 -0500

    update java script

---
+++ findyouridealdestinationquiz.js
@@ -0,0 +1,3 @@
+var navigationButton= document.getElementById ("navigateButton");
+navigateButton.addEventListener("click", function(){
+window.location.href= "http://127.0.0.1:5502/Anasdestinations.html"});
