9d28c2b56bf5b3a8f4dbbf1d9e267a4003d459b2
ec449 <ec449@njit.edu>
2024-11-21 17:39:03 -0500

    Added another JS page-Erica

--- findyouridealdestinationquiz.js
+++ findyouridealdestinationquiz.js
@@ -1,3 +1,20 @@
 var navigationButton= document.getElementById ("navigateButton");
 navigateButton.addEventListener("click", function(){
 window.location.href= "http://127.0.0.1:5502/Anasdestinations.html"});
+
+let Question1='Spring (March-May)';
+console.log("Answer 1 Ex:", Question1);
+let Question2='City ';
+console.log("Answer 2 Ex :",Question2);
+let Question3='A Weekend ';
+console.log("Answer 3 Ex :",Question3);
+let Question4= 'Car';
+console.log("Answer 4 Ex :",Question4);
+let Question5="Relaxation ";
+console.log("Answer 5 Ex :",Question5);
+let Question6='Hotel'
+console.log("Answer 6 Ex :",Question6);
+
+
+
+
