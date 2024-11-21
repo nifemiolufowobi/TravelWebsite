8ad53864a5c5ff68a979b4ed37b6ef8805bd937a
ec449 <ec449@njit.edu>
2024-11-21 17:28:19 -0500

    Erica's updates and JS pages

---
+++ profilecreation-EC.js
@@ -0,0 +1,15 @@
+let FirstName= 'Erica';
+console.log("FirstName example:", FirstName);
+let LastName= 'Carangui';
+console.log("LastName example:", LastName);
+let Username=FirstName.concat("18",LastName,"!" );
+console.log("Username example:", Username);
+let Password=5
+if(Password>4 && Password>0){
+    console.log("Password:The password is incorrect.");
+}else{
+    console.log("Password: The password is correct.");
+}
+let Email=FirstName.concat("18",LastName,"@gmail.com" );
+console.log("Email example:", Email);
+
---
+++ travelWebsite-EC.js
@@ -0,0 +1,19 @@
+let age=18;
+let Names=['Erica','Nifemi', 'Ana', 'Kayanna', 'Selvia'];
+
+console.log(age);
+console.log("Names");
+for (let i = 0; i <= 4; i++) {
+  console.log(Names[i]);
+}
+console.log("Hello, "+ Names + ", look for the perfect travel place!");
+
+let isMember=true;
+ if (isMember=true){
+    console.log("Member discount applied");
+ }
+
+
+
+
+
