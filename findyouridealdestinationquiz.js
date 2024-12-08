e0910e672af0b003a84e31ac8dd10fcdc9d7f7f5
ec449 <ec449@njit.edu>
2024-12-07 21:03:21 -0500

    Functional Quiz Erica Edits

--- findyouridealdestinationquiz.js
+++ findyouridealdestinationquiz.js
@@ -15,29 +15,5 @@ console.log("Answer 5 Ex :",Question5);
 let Question6='Hotel'
 console.log("Answer 6 Ex :",Question6);
 
-document.getElementById("quiz").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-
-    const travel = document.querySelector('input[name="travel"]:checked');
-    const season = document.querySelector('input[name="season"]:checked');
-    const destination = document.querySelector('input[name="Destinantions"]:checked');
-    const activity = document.querySelector('input[name="Activity"]:checked');
-    const budget = document.querySelector('input[name="Budget"]:checked');
-
-    if (!travel || !season || !destination || !activity || !budget) {
-        alert("Please answer all questions!");
-        return;
-      }
-      const resultCategory = `${travel.value}-${destination.value}`;
-
-      if (resultCategory.includes("Solo-Beach")) {
-        // Redirect to Solo Beach Trip Page
-        window.location.href = "http://127.0.0.1:5502/Anasdestinations.html";}
-       else {(resultCategory.includes("Family/Friends-City")) 
-        // Redirect to Family/Friends City Trip Page
-        window.location.href = "http://127.0.0.1:5502/erica's-destinationpage.html";
-      }
-  
-    });
 
     
